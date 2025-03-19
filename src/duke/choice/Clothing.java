86df151cebf8a3ba8c02be3857e40ba304fd2d0c
Leonardo <lemartinezba@unal.edu.co>
2025-03-19 11:29:00 -0500

    POO Defining classes and creating objects

---
+++ src/duke/choice/Clothing.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Clothing {
+    String description;
+    double price;
+    String size = "M";
+    
+    private final double MIN_PRICE = 10.0;
+    private final double MIN_TAX = .2;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price + (price * MIN_TAX);
+    }
+
+    public void setPrice(double price) {
+        
+        this.price = (price > MIN_PRICE) ? price : MIN_PRICE;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+}
---
+++ src/duke/choice/ShopApp.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class ShopApp {
+
+    public static void main(String[] args) {
+        double tax = 0.2, total = 0.0;
+        System.out.println("Welcome to Duke Choice Shop");
+
+        Customer c1 = new Customer();
+
+        c1.setName("Pinky");
+        c1.setSize("S");
+
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+
+        Clothing[] items = {item1, item2, new Clothing(), new Clothing()};
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+        item1.size = "M";
+
+        item2.description = "Orange T-Shirt";
+        item2.price = 10.5;
+        item2.size = "S";
+
+        items[2].description = "Green Scarf";
+        items[2].price = 5.0;
+        items[2].size = "M";
+
+        items[3].description = "Blue T-Shirt";
+        items[3].price = 10.5;
+        items[3].size = "S";
+
+//        System.out.println("Item 1" + "," + item1.description + "," + item1.price + "," + item1.size);
+//        System.out.println("Item 2" + "," + item2.description + "," + item2.price + "," + item2.size);
+//        total = (item1.price + item2.price * 2) * (1 + tax);
+//        
+//        System.out.println("The whole price is " + total);    
+        int measurement = 8;
+
+        c1.addItems(items);
+
+        c1.setSize(measurement);
+        System.out.println("Customer is " + c1.getName() + ", " + c1.getSize() + ", " + c1.getTotalClothingCost());
+        for (Clothing item : c1.getItems()) {
+            System.out.println("Items: " + item.getDescription());
+        }
+
+    }
+
+}
